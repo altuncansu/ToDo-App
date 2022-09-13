61915fd2f728f0060e604a6206263c49d2508155
Cansu Altun <cansu.altun@coino.local>
2022-09-13 17:44:40 +0300

    scssentegration

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,39 +1,44 @@
-import React , {useState} from 'react';
-import './App.css';
-import { ITask } from "./Interfaces";
+import React , {useState , FC , ChangeEvent} from 'react';
+import "./app.scss";
+import {ITask} from "./Interfaces";
+import TodoTask from "./components/TodoTask";
+import {FiEdit} from "@react-icons/all-files/fi/FiEdit";
 
-const App : React.FC = () =>  {
-  const [task,setTask] = useState<string>("");
-  const [deadline , setDeadline]  = useState<number>(0);
-  const [todoList, setTodoList] = useState([]);
+const App : FC = () =>  {
+     const [task,setTask] = useState<string>(""); //define state with type
+     const [deadline , setDeadline]  = useState<number>(0);
+     const [todoList, setTodoList] = useState<ITask[]>([]);
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-        if (event.target.name === "task") {
-            setTask(event.target.value);
-        } else {
-            setDeadline(Number(event.target.value));
-        }
-    };
-
-
+ const handleChange = (event : ChangeEvent<HTMLInputElement> ) : void => {
+       if(event.target.name === "task") {
+           setTask(event.target.value)
+       } else {
+           setDeadline(Number(event.target.value)) //Change type wrapping with Number + paranthesis
+       }
+    }
+    const addTask = ():void => {
+     const newTask = {taskName : task , deadline:deadline}
+        setTodoList([...todoList,newTask]) //Adding newTask split on todoList
+        setTask("");
+        setDeadline(0);
+    }
     return (
     <div className="App">
       <div className="container">
-        <h1 className="header">TodoList App</h1>
-        <input type="text" placeholder="Task" className="place" name:"task" value={task} onChange={handleChange} />
+        <h1 className="header">TodoList App <FiEdit/></h1>
+        <input type="text" placeholder="Task" className="place" name="task" value={task} onChange={handleChange} />
           <div>
-        <input type="number" placeholder="Deadline" className="deadline" name:"deadline" value={deadline} onChange={handleChange} />
+        <input type="number" placeholder="Deadline" className="deadline" name="deadline"  value={deadline} onChange={handleChange} />
         </div>
           <div>
           <button type="submit" className="btn">Add Task</button>
           </div>
          <div className="todoList">
-          <ul>
-            <li></li>
-          </ul>
+             {todoList.map((task:ITask, key : number) => {
+                 return <TodoTask key={key} task={task}   />;
+             })}
         </div>
       </div>
-
     </div>
   );
 }
---
+++ my-app/src/components/TodoTask.tsx
@@ -0,0 +1,13 @@
+import React from "react" ;
+import {ITask} from "../Interfaces";
+interface Props {
+   task:ITask;
+}
+const TodoTask = ({task}:Props) => {
+    return (
+        <div>
+            {task.taskName}{task.deadline}
+        </div>
+    )
+}
+export default TodoTask;
